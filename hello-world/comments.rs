81a1bde3828c8615e4f0ddf7815a3b59f06f639b
MadeInShineA <olivier.amacker@netplus.ch>
2024-12-10 21:21:46 +0100

    Began the Hello World section

---
+++ hello-world/comments.rs
@@ -0,0 +1,12 @@
+fn main() {
+    // This is a single line comment
+
+    /*
+       This is a
+       multi-line
+       comment
+    */
+
+    let x = 5 + 90 + 5;
+    println!("Is 'x' 10 or 100? x = {}", x)
+}
---
+++ hello-world/prints.rs
@@ -0,0 +1,34 @@
+fn main() {
+    println!(
+        "This is the first argument {} this is the second one {}",
+        1, 2
+    );
+
+    println!(
+        "This is the second argument {1} this is the first one {0}",
+        1, 2
+    );
+
+    println!(
+        "This is the cool argument {cool} this is the bad one {bad}",
+        cool = "yay",
+        bad = "nooo"
+    );
+
+    // This doesn't work
+    // println!("{number:padding_char$>number_to_pad_to$}", number=1, padding_char='a', number_to_pad_to = 10);
+
+    println!("Binary {:b}", 1234);
+
+    let name: &str = "MadeIn";
+    let surname: &str = "ShineA";
+    println!("{name} {surname}");
+
+    // Activities
+
+    println!("My name is {0}, {1} {0}", "Bond", "James");
+
+    let pi: f64 = 3.141592;
+    println!("Pi is roughly {pi:.3}");
+
+}
